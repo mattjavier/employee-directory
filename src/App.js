d767bddb62f37ee045c6dfba8a603fa22b6c81b1
Matt Javier <matthewrjavier@gmail.com>
2020-09-17 10:24:55 -0700

    created table, sorting complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
+import axios from 'axios'
+import Table from './components/Table'
 
 const App = () => {
-  return (
 
+  const [employeeState, setEmployeeState] = useState({
+    employees: []
+  })
+
+  useEffect(() => {
+    axios.get('https://randomuser.me/api?results=20')
+      .then(({ data }) => {
+        setEmployeeState({ ...employeeState, employees: data.results })
+      })
+      .catch(err => console.error(err))
+  }, [])
+
+  const data = employeeState.employees
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Employee',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'name.first',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'name.last'
+          }
+        ]
+      },
+      {
+
+      }
+    ],
+    []
+  )
+
+  console.log(data)
+  return (
+    <>
+      <Table columns={columns} data={data} />
+    </>
   )
 }
 
